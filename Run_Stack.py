e3fb182218d8a12a770f24cfd310c2c34e3ca97e
nervign <nervign@nervlite.com>
2020-07-10 21:34:44 +0100

    Optimised Stack Solution

--- Run_Stack.py
+++ Run_Stack.py
@@ -6,22 +6,21 @@ symbol_pair = {
     '(':')'
 }
 openers = symbol_pair.keys()
+
 def is_balanced(symbol_list):
     int_stack = Stack()
     
     for symbol in symbol_list:
-        if int_stack.is_empty():
-            int_stack.push(symbol)            
+        if symbol in openers:
+            int_stack.push(symbol)
         else:
-            if symbol in openers:
-                int_stack.push(symbol)                
+            if int_stack.is_empty():
+                return False                   
             else:
                 ol = int_stack.pop()               
-                if symbol_pair[ol] == symbol:
-                    continue
-                else:
-                    return False
-    
+                if symbol_pair[ol] != symbol:
+                    return False 
+                    
     return int_stack.is_empty()
 
 print(is_balanced(['{','}','[',']', '(',')']))
